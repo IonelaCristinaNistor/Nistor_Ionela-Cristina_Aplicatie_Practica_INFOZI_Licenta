ab308362e7e2fc189cd26c310b1a80ea55c9dd1f
IonelaCristinaNistor <nistorc71@gmail.com>
2024-06-18 02:58:01 +0300

    database

--- djangoproj/djangoproj/settings.py
+++ djangoproj/djangoproj/settings.py
@@ -133,8 +133,12 @@ WSGI_APPLICATION = 'djangoproj.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'licenta_2024',
+        'USER': 'postgres',
+        'PASSWORD': 'password',
+        'HOST': 'localhost',
+        'PORT': '5432'
     }
 }
 
